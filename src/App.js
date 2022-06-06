a491122cda98804742e9ef6c6181d9a877d6fb44
javohirglabov <javohirglabov74@gmail.com>
2022-06-06 17:11:24 +0500

    Created Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Employee} from "./components/Employee";
+import './sass/main.scss';
+
+function App() {
+  return (
+    <>
+      <Employee/>
+    </>
+  );
+}
+
+export default App;
